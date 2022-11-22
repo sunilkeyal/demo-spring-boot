5068d3d1ba46540ef32d12b92f74aa96f87d55e0
Sunil Keyal <sunilkeyal@hotmail.com>
2022-11-22 11:44:56 -0500

    Updated library versions

--- src/main/java/com/example/DemoSpringBootApplication.java
+++ src/main/java/com/example/DemoSpringBootApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class DemoSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(DemoSpringBootApplication.class, args);
 	}
 }
--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -4,21 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Random;
-
 @RestController
 @Slf4j
 public class HelloController {
 
-	@RequestMapping("/test")
-	public String index() {
-		log.info("Hello World test metbod " + Math.random());
-		return "Greetings";
-	}
-
 	@RequestMapping("/hello")
 	public String hello() {
-		log.info("Hello World hello mwthod " + Math.random());
-		return "Hello Sunil";
+		String random = "Hello World " + Math.random();
+		log.info(random);
+		return random;
 	}
 }
