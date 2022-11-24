a5801be5ca52412bea8e970a3b86a1606dc2d83b
Sunil Keyal <sunilkeyal@users.noreply.github.com>
2022-11-24 10:49:44 -0500

    Update DemoSpringBootApplication.java

--- src/main/java/com/example/DemoSpringBootApplication.java
+++ src/main/java/com/example/DemoSpringBootApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoSpringBootApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringBootApplication.class, args);
 	}
